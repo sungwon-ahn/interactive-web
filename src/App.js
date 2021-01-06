38045f59d5e465899fc149f8cd9aa31a59eae41e
sungwon-ahn <sungwon.ann@gmail.com>
2021-01-06 14:20:21 +0000

    apply card flipped

--- src/App.js
+++ src/App.js
@@ -1,14 +1,43 @@
 import React, { Component } from 'react';
-import './App.css'
+import ReactCardFlip from 'react-card-flip';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        react
-      </div>
-    );
-  }
+class App extends Component{
+    constructor(){
+        super();
+        this.state={
+            isFlipped: false
+        }
+    }
+    handleClick=()=>{
+        this.setState(prevState => ({ isFlipped: !prevState.isFlipped }));
+    }
+    state = {
+       count: 0
+    };
+
+    render(){
+        return (
+            <ReactCardFlip isFlipped={this.state.isFlipped} flipDirection="vertical">
+                <div style={{
+                    background: "aqua",
+                    fontSize: "50px",
+                    color: "black",
+                }}>
+                This is the front.<br></br>
+                <button onClick={this.handleClick}>Click</button>
+                </div>
+        
+                <div style={{
+                    background: "black",
+                    fontSize: "50px",
+                    color: "aqua",
+                }}>
+                This is the back.<br></br>
+                <button onClick={this.handleClick}>Click</button>
+                </div>
+            </ReactCardFlip>
+        );
+    }
 }
 
 export default App;
